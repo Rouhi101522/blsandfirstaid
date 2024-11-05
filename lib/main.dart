04f3e2763d2d853621e71c594ac16ef0bbddfc36
rouh <jgcuhit@up.edu.ph>
2024-11-05 15:57:34 +0800

    Bug found, Unable to see assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'splash_screen.dart'; // Import the splash screen
+import 'splash_screen.dart'; 
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const SplashScreen(), // Set the splash screen as the home
+      home: const SplashScreen(), 
     );
   }
 }
